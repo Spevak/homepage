970947f4a25dfdd2ca157f6cc8e5857b1e627c23
Kevin Spevak <kevin@vidora.com>
2015-06-05 14:32:59 -0700

    Added method for returning deleted body

--- www/www.spevak.me/testing/script.js
+++ www/www.spevak.me/testing/script.js
@@ -1,9 +1,16 @@
-var body = $('body')
-
 function testo() {
   console.log($('body'))
 }
 
 function removeBody() {
   $('body').remove()
+}
+
+function index() {
+  $('body').remove();
+  $('html').append(indexBody);
+}
+
+$(document).ready(function() {
+  var indexBody = $('body');
 }
