c04f0532ea4a47abd91c84c26d1cf11b35250d10
Kevin Spevak <kevin@vidora.com>
2015-06-05 14:48:06 -0700

    Added function for creating dom elements

--- www/www.spevak.me/testing/script.js
+++ www/www.spevak.me/testing/script.js
@@ -12,3 +12,17 @@ function index() {
 $(document).ready(function() {
   indexBody = $('body');
 });
+
+//Create an elmement
+function el(type, id, classes, children) {
+  var element = $('<' + type + '>' + '</' + type + '>')
+  if (id) {
+    element.attr('id', id);
+  }
+  if (classes) {
+    element.attr('class', classes);
+  }
+  for (var i = 0; i < children.length; i++) {
+    element.append(children[i]);
+  }
+}
